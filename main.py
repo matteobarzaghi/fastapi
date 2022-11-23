e7fd932b69b311a05ee2367587bc64ab00181aa9
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-11-23 20:56:14 +0100

    Creation of first post request with body

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 # decorator @app + url
-@app.get("/welcomeMessage")
+@app.get("/")
 def getHelloWorld():
     return {"message" : "Hello world"}
+
+@app.get("/welcomeMessage")
+def getHelloWorld():
+    return {"message" : "Welcome to this FAST API tutorial"}
+
+# first post call, payload define a dict and import Body from fastapi
+@app.post("/createPosts")
+def createPosts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post" : f"title {payLoad['title']} content: {payLoad['content']}"}
