169513e8f03ff79a09fba0912f53e8f305ec911d
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-11-26 09:47:36 +0100

    Hard coded some background posts to retrieve via CRUD operations

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import Body, FastAPI
 from pydantic import BaseModel 
 
 app = FastAPI()
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": "2"}, 
+{"title": "title of post 1", "content": "content of post 1", "id": "2"}]
 
 # validation schema for post
 class Post(BaseModel):
@@ -23,7 +25,12 @@ def getHelloWorld():
     return {"message" : "Welcome to this FAST API tutorial"}
 
 # first post call, payload define a dict and import Body from fastapi
-@app.post("/createPosts")
-def createPosts(new_post: Post):
-    print(new_post.title)
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post.dict())
     return {"data" : "A new post has been created"}
+
+# first post call, payload define a dict and import Body from fastapi
+@app.get("/posts")
+def get_posts():
+    return {"data" : my_posts}
