ff7ef7b54c92f0087a8347be757a6ad7dc6e84e7
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-11-28 18:06:59 +0100

    implemented single post get

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from random import randrange
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel 
 
 app = FastAPI()
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": "2"}, 
-{"title": "title of post 1", "content": "content of post 1", "id": "2"}]
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "title of post 2", "content": "content of post 2", "id": 2}, ]
 
 # validation schema for post
 class Post(BaseModel):
@@ -14,6 +14,11 @@ class Post(BaseModel):
     published: bool = False
     #or with the Optional object
     rating: Optional[int] = None
+
+def find_post_by_id(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p 
 
 # decorator @app + url
 @app.get("/")
@@ -27,10 +32,20 @@ def getHelloWorld():
 # first post call, payload define a dict and import Body from fastapi
 @app.post("/posts")
 def create_posts(post: Post):
-    print(post.dict())
-    return {"data" : "A new post has been created"}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,100000)
+    my_posts.append(post_dict)
+    return {"data" : post_dict}
 
 # first post call, payload define a dict and import Body from fastapi
 @app.get("/posts")
 def get_posts():
     return {"data" : my_posts}
+
+
+@app.get("/posts/{id}")
+# inside the function parameter I state that id must be an int
+def get_post(id : int):
+    # instead of manually convert it id to int
+    post = find_post_by_id(id)
+    return {"data" : post}
