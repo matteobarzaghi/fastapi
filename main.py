75a058a400200e208588c80cb822ce64de93898f
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-12-02 16:02:58 +0100

    Implemented single post deleting

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def find_post_by_id(id):
         if p["id"] == id:
             return p 
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i 
+
+
 # decorator @app + url
 @app.get("/")
 def getHelloWorld():
@@ -54,3 +60,12 @@ def get_post(id : int, response: Response):
         #response.status_code = status.HTTP_404_NOT_FOUND
         #return {'message' : f"id: {id} was not found!"}
     return {"data" : post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"post with id {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
