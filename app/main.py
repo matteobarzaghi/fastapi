3ffa32fd4bd9ac690526664b1a85aa567c4cb7c2
Matteo Barzaghi <matteo.barzaghi91@gmail.com>
2022-12-16 15:37:41 +0100

    Implemented crud operations and got rid of hard coded data

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ from psycopg2.extras import RealDictCursor
 import time
 
 app = FastAPI()
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {
-    "title": "title of post 2", "content": "content of post 2", "id": 2}, ]
 
 # validation schema for post
 
@@ -20,6 +18,8 @@ class Post(BaseModel):
     published: bool = False
     # or with the Optional object
     #rating: Optional[int] = None
+
+# DATABASE CONNECTION
 
 
 while True:
@@ -40,71 +40,67 @@ while True:
         time.sleep(2)
 
 
-def find_post_by_id(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
-
 # decorator @app + url
 @app.get("/")
 def getHelloWorld():
     return {"message": "Hello world"}
 
 # first post call, payload define a dict and import Body from fastapi
-
-
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 100000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
-
-# first post call, payload define a dict and import Body from fastapi
+    cursor.execute(""" INSERT INTO posts (title, content) VALUES (%s, %s) RETURNING * """,
+                   (post.title, post.content))
+    new_post = cursor.fetchone()
+    # to save data in the db we have to reference the connection and do a commit
+    conn.commit()
+    return {"data": new_post}
 
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute(""" SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 
 @app.get("/posts/{id}")
 # inside the function parameter I state that id must be an int
 def get_post(id: int, response: Response):
-    # instead of manually convert it id to int
-    post = find_post_by_id(id)
-    if not post:
+    # apparently the comma after str(id) is SUPER important for SOME reason
+    cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
+    post_with_id = cursor.fetchone()
+    if not post_with_id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {id} was not found")
-        #response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'message' : f"id: {id} was not found!"}
-    return {"data": post}
+                            detail=f"post with id {id} does not exist")
+    return {"data": post_with_id}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
-    if index == None:
+
+    cursor.execute(
+        """ DELETE FROM posts WHERE id = %s RETURNING * """, (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} does not exist")
-    my_posts.pop(index)
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+
+    cursor.execute(""" UPDATE posts SET title =%s, content=%s RETURNING * """,
+                   (post.title, post.content))
+    updated_post = cursor.fetchone()
+    # to save data in the db we have to reference the connection and do a commit
+    conn.commit()
+
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id {id} does not exist")
     post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
+
     return Response(status_code=status.HTTP_202_ACCEPTED)
